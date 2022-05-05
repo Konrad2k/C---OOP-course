1d19c32577311f7a883b51bb624ead94c0dde3cc
Konrad2k <konrad.hoza@op.pl>
2022-05-05 15:36:57 +0200

    Exercise 2

---
+++ Ex_2/Ex_2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Print {
+public:
+    void print(int i) {
+
+        cout << "Metoda wywolana zostala dla obiektu numer " << i << endl;
+    }
+    // Miejsce na metodę
+};
+
+int main() {
+    Print p1, p2;
+
+    p1.print(1);
+    p2.print(2);
+
+    // Miejsce na obiekty i wywołanie metod
+    system("pause");
+}
