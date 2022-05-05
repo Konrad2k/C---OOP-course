f397d138a7a6673ea5592471a30dfb39c65b14e6
Konrad2k <konrad.hoza@op.pl>
2022-05-05 15:32:02 +0200

    exercise 1

---
+++ Ex_1/Ex_1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Print {
+public:
+    void print() {
+        cout << "Korzystamy z klasy o nazwie Print";
+    }
+    // Miejsce na metodę
+};
+
+int main() {
+    Print p1;
+
+    p1.print();
+    // Miejsce na obiekt oraz wywołanie metod
+    system("pause");
+}
