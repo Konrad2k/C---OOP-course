8cca99c2b235ae4dce2f5af2b309c279f581ef26
Konrad2k <konrad.hoza@op.pl>
2022-05-06 23:51:34 +0200

    exercise 4

---
+++ Ex_4/Ex_4.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Print {
+private:
+    int number = 5;
+    // Miejsce na atrybut
+public:
+    void print() {
+        cout << number;
+    }
+    // Miejsce na metodę
+};
+
+int main() {
+    Print p1;
+    p1.print();
+    // Miejsce na obiekt i wywołanie metody
+    return 0;
+}
