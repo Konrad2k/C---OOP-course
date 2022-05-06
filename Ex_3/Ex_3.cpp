d20aa5c9540dc017dd628b7f27a1deb558f370b4
Konrad2k <konrad.hoza@op.pl>
2022-05-06 23:50:17 +0200

    exercise 3

---
+++ Ex_3/Ex_3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Print {
+public:
+    int number = 5;
+    // Miejsce na atrybut
+};
+
+int main() {
+
+    Print p1;
+    cout << p1.number;
+    // Miejsce na obiekt
+    return 0;
+}
