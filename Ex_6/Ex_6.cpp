653cf321fffe51010fb8caa01e0188d68258ebff
Konrad2k <konrad.hoza@op.pl>
2022-05-07 23:57:04 +0200

    exercise 6

---
+++ Ex_6/Ex_6.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Sum {
+private:
+    int number_1 = 5;
+    int number_2 = 10;
+    // Miejsce na atrybuty
+public:
+    void sumValues() {
+        cout << number_1 + number_2;
+    }
+    // Miejsce na metodę
+};
+
+int main() {
+    Sum s1;
+    s1.sumValues();
+    // Miejsce na obiekt i wywołanie metody
+    return 0;
+}
