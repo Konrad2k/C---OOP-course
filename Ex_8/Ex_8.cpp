67b85191d957803bd0829c59952e25249a51bccb
Konrad2k <konrad.hoza@op.pl>
2022-05-08 21:03:06 +0200

    exercise 8

---
+++ Ex_8/Ex_8.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Print {
+public:
+    void print(string nazwa) {
+        cout << nazwa << endl;
+    }
+    // Miejsce na metodę
+};
+
+int main() {
+    Print p1, p2;
+
+    p1.print("Programowanie obiektowe");
+    p2.print("Unreal Engine");
+    // Miejsce na obiekty i wywołanie metod
+    return 0;
+}
