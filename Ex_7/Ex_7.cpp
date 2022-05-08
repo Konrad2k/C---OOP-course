4b862442c36539045145985ce1b2aaca7eba2de0
Konrad2k <konrad.hoza@op.pl>
2022-05-08 21:02:14 +0200

    exercise 7

---
+++ Ex_7/Ex_7.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Print {
+private:
+    string words = "Programowanie obiektowe";
+    // Miejsce na atrybut
+public:
+    void print() {
+        cout << words;
+    }
+    // Miejsce na metode
+};
+
+int main() {
+    Print p1;
+    p1.print();
+    // Miejsce na obiekt i wywołanie metody
+    return 0;
+}
