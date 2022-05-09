e4ae768eb69d5a78c69bb8a1e75544461c53c083
Konrad2k <konrad.hoza@op.pl>
2022-05-09 20:38:27 +0200

    Exercise 9

---
+++ Ex_9/Ex_9.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Ceny {
+private:
+    float cena;
+    // Miejsce na atrybut
+public:
+    void setValues(float wartosc) {
+        cena = wartosc;
+    }
+
+    void print() {
+        cout << cena << endl;
+    }
+    // Miejsce na metody
+};
+
+int main() {
+    Ceny c1[5];
+
+    c1[0].setValues(15.49);
+    c1[0].print();
+    c1[1].setValues(45.99);
+    c1[1].print();
+    // Miejsce na obiekt i wywołanie metod
+    return 0;
+}
