2b8d25087a2e58abd6d282b44e5c6a0047ae7e8f
Konrad2k <konrad.hoza@op.pl>
2022-05-11 23:59:54 +0200

    exercise 12

---
+++ Ex_12/Ex_12.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Print {
+public:
+
+    int number(int liczba) {
+        cout << liczba;
+    }
+
+    int words(string slowa) {
+        cout << slowa;
+
+    }
+    // Miejsce na atrubuty
+};
+
+int main() {
+    Print p1;
+    p1.number(15);
+    cout << endl;
+    p1.words("Object Oriented Programming");
+    // Miejsce na kod
+    return 0;
+}
