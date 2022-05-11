de70c0d6cd2bf4b669041507d2afd1b558c5f641
Konrad2k <konrad.hoza@op.pl>
2022-05-11 23:59:14 +0200

    exercise 11

---
+++ Ex_11/Ex_11.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+class Print {
+public:
+    void display() {
+        cout << "Deep Learning";
+    }
+};
+// Miejsce na klase
+
+int main() {
+    Print p1;
+    p1.display();
+    // Miejsce na kod
+    return 0;
+}
