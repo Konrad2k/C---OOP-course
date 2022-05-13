3dc2a78653a34acb20dc594802a48efc1d491020
Konrad2k <konrad.hoza@op.pl>
2022-05-13 20:49:07 +0200

    exercise 14

---
+++ Ex 14/Ex 14.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#define PI (3.14)
+// Miejsce na stałą
+
+using namespace std;
+
+class Kolo {
+private:
+    float promien;
+public:
+    void poleKola(float r) {
+        cout << "Pole kola o promieniu " << r << " wynosi: " << (r * r) * PI << endl;
+    }
+    // Miejsce na atrybuty i metody
+};
+
+int main() {
+    Kolo k1, k2;
+
+    k1.poleKola(5);
+    k2.poleKola(3);
+    // Miejsce na obiekty
+    system("pause");
+}
