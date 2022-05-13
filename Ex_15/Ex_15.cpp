5a141a7e2190c66c567bd3b5496099f5c13b2a40
Konrad2k <konrad.hoza@op.pl>
2022-05-13 20:50:24 +0200

    exercise 15

---
+++ Ex_15/Ex_15.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Rower {
+private:
+    string marka;
+    string rozmiar_ramy;
+    float rozmiar_kola;
+    string kolor;
+    float cena;
+    // Miejsce na atrybuty
+public:
+    void pobieranieDanych(string p_marka, string p_rozmiar_ramy, float p_rozmiar_kola, string p_kolor, float p_cena) {
+        marka = p_marka;
+        rozmiar_ramy = p_rozmiar_ramy;
+        rozmiar_kola = p_rozmiar_kola;
+        kolor = p_kolor;
+        cena = p_cena;
+    }
+    void wyswietlanieDanych() {
+        cout << "Marka: " << marka << endl;
+        cout << "Rozmiar ramy: " << rozmiar_ramy << endl;
+        cout << "Rozmiar kola: " << rozmiar_kola << endl;
+        cout << "Kolor: " << kolor << endl;
+        cout << "Cena: " << cena << " PLN" << endl << endl;
+    }
+    // Miejsce na metody
+
+};
+
+int main() {
+    Rower r1, r2;
+    r1.pobieranieDanych("Kross", "L", 27.5, "Czarny", 1650);
+    r1.wyswietlanieDanych();
+    r2.pobieranieDanych("Scott", "XL", 29, "Czerwony", 1900);
+
+    r2.wyswietlanieDanych();
+    // Miejsce na obiekty
+
+    system("pause");
+}
