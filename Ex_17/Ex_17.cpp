33ea7d91ff8c603388f1bce071a81210a7de4b3f
Konrad2k <konrad.hoza@op.pl>
2022-05-17 21:12:30 +0200

    exercise 17

---
+++ Ex_17/Ex_17.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Numbers {
+private:
+	vector <int> numbers;
+	// Miejsce an atrybut
+public:
+	void setNumbers(int numer1, int numer2, int numer3) {
+		numbers.push_back(numer1);
+		numbers.push_back(numer2);
+		numbers.push_back(numer3);
+	}
+	void displayNumbers() {
+		for (int i = 0; i < numbers.size(); i++) {
+			cout << "|" << numbers[i];
+		}
+		cout << "|";
+	}
+	// Miejsce na metody
+};
+
+int main() {
+	Numbers n1, n2;
+	n1.setNumbers(5, 3, 4);
+	n1.displayNumbers();
+	cout << endl;
+	n2.setNumbers(7, 4, 2);
+	n2.displayNumbers();
+	return 0;
+}
